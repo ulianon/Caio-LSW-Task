dea1da9e49ba0ba4331687c93e7fa2fe7362557a
ulianon <lianon@gmail.com>
2023-08-20 23:12:02 -0300

    Starting animation states
    Initial grounds for animation and netclients

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : NetworkBehaviour
 {
     public static GameManager Instance;
 
+
+    public Canvas currentCanvas;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+
+    //Network regions
+    public void MyStartClient()
+    {
+        NetworkManager.Singleton.StartClient();
+        currentCanvas.enabled = false;
+    }
+
+    public void MyStartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+        currentCanvas.enabled = false;
     }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 public class Player : NetworkBehaviour
 {
 
+    [SerializeField] private GameObject gameCam;
+
     [Header("Physics")]
-    private Rigidbody2D MyRigidBody;
+    [SerializeField] private Rigidbody2D m_RigidBody;
 
 
     //Movement related variables and methods
@@ -15,20 +17,39 @@ public class Player : NetworkBehaviour
     [SerializeField] float playerSpeed;
     float moveHorizontal;
     float moveVertical;
+    bool isPlayerMoving;
     Vector2 mouseTargetPos;
 
     //Player States
     [Header("PlayerStates")]
     [SerializeField] string currentState;
     [SerializeField] string previousState;
-    [SerializeField] enum PlayerStates { };
+    [SerializeField]
+    enum PlayerStates
+    {
+        Idle, Walk, Run, Death, Attack
+    };
+    PlayerStates m_playerStates = PlayerStates.Idle;
+
+    //Player Animations
+    [Header("Animation")]
+    [SerializeField] Animation state1;
+    [SerializeField] Animator m_Animator;
+    [SerializeField]
+    enum PlayerAnimationStates
+    {
+        Idle, Walk, Run, Death, Attack, Hit
+    }
+    PlayerAnimationStates m_playerAnimationStates = PlayerAnimationStates.Idle;
+
 
     //Customizable inputs
     [SerializeField] enum PlayerInputs { };
 
     private void Awake()
     {
-        MyRigidBody = GetComponent<Rigidbody2D>();
+        if (!IsOwner) return;
+
     }
 
 
@@ -41,18 +62,25 @@ public class Player : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!IsOwner)
+        {
+            gameCam.SetActive(false);
+            return;
+        }
         GetPlayerInputs();
+        PlayerStateMachine();
     }
 
     private void FixedUpdate()
     {
-        MovePlayer();
+        if (!IsOwner) return;
+        if (isPlayerMoving) MovePlayer();
     }
 
     void GetPlayerInputs()
     {
-        moveHorizontal = Input.GetAxis("Horizontal");
-        moveVertical = Input.GetAxis("Vertical");
+        //moveHorizontal = Input.GetAxis("Horizontal");
+        //moveVertical = Input.GetAxis("Vertical");
 
         if (Input.GetMouseButton(0))
         {
@@ -60,29 +88,45 @@ public class Player : NetworkBehaviour
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
             {
                 mouseTargetPos = new Vector2(hit.point.x, hit.point.y);
-                
+                m_playerStates = PlayerStates.Walk;
             }
         }
     }
+
     #region Movement
     void MovePlayer()
     {
         Vector2 current2dPos = new Vector2(transform.position.x, transform.position.y);
-        MyRigidBody.MovePosition(Vector2.Lerp(current2dPos, mouseTargetPos, playerSpeed/1000f));
-
-        Vector3 TestCommitomgchanged;
+        if (current2dPos.sqrMagnitude != mouseTargetPos.sqrMagnitude)
+            m_RigidBody.MovePosition(Vector2.Lerp(current2dPos, mouseTargetPos, playerSpeed / 1000f));
+        else
+        {
+            isPlayerMoving = false;
+            m_Animator.SetBool("Walk", false);
+        }
         //MyRigidBody.velocity = new Vector2(moveHorizontal * playerSpeed, moveVertical * playerSpeed);
     }
-
     #endregion
-    void MovePlayer23()
-    {
-        Vector2 current2dPos = new Vector2(transform.position.x, transform.position.y);
-        MyRigidBody.MovePosition(Vector2.Lerp(current2dPos, mouseTargetPos, playerSpeed / 1000f));
 
-        Vector3 TestCommitomgchanged;
-        //MyRigidBody.velocity = new Vector2(moveHorizontal * playerSpeed, moveVertical * playerSpeed);
-    }
     #region PlayerStates
+    void PlayerStateMachine()
+    {
+        switch (m_playerStates)
+        {
+            case PlayerStates.Idle:
+                break;
+            case PlayerStates.Run:
+                break;
+            case PlayerStates.Walk:
+                m_Animator.SetBool("Walk", true);
+                isPlayerMoving = true;
+                break;
+            case PlayerStates.Death:
+                break;
+            case PlayerStates.Attack:
+                break;
+        }
+    }
+
     #endregion
 }
